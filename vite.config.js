d3117c1081f3139cdede3ba102212a62965e07d2
Itsuki-Okada <oitsuki0110@gmail.com>
2025-05-30 16:07:08 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// frontend/vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: ".", // 明示的に現在のディレクトリ
   plugins: [react()],
 });
