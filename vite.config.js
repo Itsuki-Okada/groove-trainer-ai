54a8edabc6ba89e1e52dd408fc630b462ade9a97
Itsuki-Okada <oitsuki0110@gmail.com>
2025-05-30 16:14:23 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+// frontend/vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: ".",
   plugins: [react()],
+  root: ".",
+  build: {
+    rollupOptions: {
+      input: "/index.html",
+    },
+  },
 });
