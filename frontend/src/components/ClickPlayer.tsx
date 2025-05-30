4999ba8f24557011350749e5d74ea00ef7e699ed
Itsuki-Okada <oitsuki0110@gmail.com>
2025-05-30 18:35:11 +0900

    Update ClickPlayer.tsx

--- frontend/src/components/ClickPlayer.tsx
+++ frontend/src/components/ClickPlayer.tsx
@@ -1,19 +1,13 @@
-import React from "react";
+import React from "react"
 
 const ClickPlayer: React.FC = () => {
-  const handleClick = () => {
-    const click = new Audio("/click.wav");
-    click.play();
-  };
+  const play = () => {
+    const click = new Audio("/click.wav")
+    click.currentTime = 0
+    click.play()
+  }
 
-  return (
-    <button
-      onClick={handleClick}
-      className="px-4 py-2 bg-blue-500 text-white rounded"
-    >
-      単発クリック音再生
-    </button>
-  );
-};
+  return <button onClick={play}>単発クリック音</button>
+}
 
-export default ClickPlayer;
+export default ClickPlayer
